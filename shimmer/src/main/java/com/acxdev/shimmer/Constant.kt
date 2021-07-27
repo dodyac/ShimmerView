5df40c2e3ff0847c225e473e02252db526eb30a4
Ahmad Dody <ahmad.ch@students.amikom.ac.id>
2021-07-27 10:20:27 +0700

    Initial commit

---
+++ shimmer/src/main/java/com/acxdev/shimmer/Constant.kt
@@ -0,0 +1,14 @@
+package com.acxdev.shimmer
+
+import android.graphics.Color
+
+object Constant {
+
+    const val USE_GRADIENT_DEFAULT = false
+    const val CORNER_DEFAULT = 16
+    const val CHILD_COUNT_DEFAULT = 3
+    const val ANGLE_DEFAULT = 20
+    const val MIN_WEIGHT = 0.0f
+    const val MAX_WEIGHT = 1.0f
+    val COLOR_DEFAULT_GRADIENT = Color.rgb(245, 245, 245)
+}
